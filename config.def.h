fc48b4f218c543275174d80ba8356fcfd8c74a78
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2024-09-14 22:30:58 +0800

    style: format config.def.h and config.h

--- config.def.h
+++ config.def.h
@@ -62,8 +62,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *browsercmd[] = {"firefox", NULL};
-static const char *lockscreencmd[] = {"betterlockscreen", "-l", NULL};
+static const char *browsercmd[] = { "firefox", NULL };
+static const char *lockscreencmd[] = { "betterlockscreen", "-l", NULL };
 
 #include "movestack.c"
 static const Key keys[] = {
@@ -112,7 +112,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 };
 
 /* button definitions */
